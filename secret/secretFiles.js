07df049f487af2ddf9a895c85df12fea74732774
Elizabeth Emmanuel <elizabethemmanuel@Elizabeths-MacBook-Pro.local>
2018-03-24 10:55:19 -0500

    update

---
+++ secret/secretFiles.js
@@ -0,0 +1,13 @@
+module.exports = {
+   facebook: {
+        clientID: '2396271197265199',
+        clientSecret: '9f38cfc34ea419edb11bfa9e14dbe0fd',
+        
+   },
+   
+   google: {
+        clientID: '981058881737-33jnq1s7s6lkapis7valuf6t710qk98r.apps.googleusercontent.com',
+        clientSecret: 'v1fS1vpaUSnF5hdH-nE_1bZk'
+   },
+   
+}
